3dd371af83b23ce4bdc821dc8a28250738a9642d
Prabhat Singh Tomar <iampstomar@gmail.com>
2020-10-27 23:35:47 +0530

    Solved using DP

--- Leetcode/Regular_Expression_Matching.py
+++ Leetcode/Regular_Expression_Matching.py
@@ -39,3 +39,24 @@
 # s contains only lowercase English letters.
 # p contains only lowercase English letters, '.', and '*'.
 # It is guaranteed for each appearance of the character '*', there will be a previous valid character to match.
+
+# Solution using Dynamic Programming (Top Down Variation)
+
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        memo = {}
+        def dp(i, j):
+            if (i, j) not in memo:
+                if j == len(p):
+                    ans = i == len(s)
+                else:
+                    first_match = i < len(s) and p[j] in {s[i], '.'}
+                    if j+1 < len(p) and p[j+1] == '*':
+                        ans = dp(i, j+2) or first_match and dp(i+1, j)
+                    else:
+                        ans = first_match and dp(i+1, j+1)
+
+                memo[i, j] = ans
+            return memo[i, j]
+
+        return dp(0, 0)
