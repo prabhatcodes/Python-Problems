29fbe5279992d792867badf86acb6bf7a9b8606e
Prabhat Singh Tomar <iampstomar@gmail.com>
2020-11-20 23:42:22 +0530

    Solving

---
+++ GFG-DSA/smallest-divisible-number.py
@@ -0,0 +1,10 @@
+# Your Task:
+# You dont need to read input or print anything. Complete the function getSmallestDivNum() which takes N as input parameter and returns the smallest number evenly divisible by each number from 1 to n.
+#
+#
+# Expected Time Complexity: O(N)
+# Expected Auxiliary Space: O(1)
+#
+#
+# Constraints:
+# 1 ≤ N ≤ 25
