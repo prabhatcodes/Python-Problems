ca191d79a9fa6b6a697baf86cee86f52403ab13b
Prabhat Singh Tomar <iampstomar@gmail.com>
2020-12-03 23:49:22 +0530

    Solved versions problem

--- Leetcode/binary-tree-inorder-traversal.py
+++ Leetcode/binary-tree-inorder-traversal.py
@@ -55,3 +55,17 @@ class Solution: # with recursion
         result = []
         helper(root)
         return result
+
+# iterative solution
+
+def inorderTraversal(self, root):
+    res, stack = [], []
+    while True:
+        while root:
+            stack.append(root)
+            root = root.left
+        if not stack:
+            return res
+        node = stack.pop()
+        res.append(node.val)
+        root = node.right
