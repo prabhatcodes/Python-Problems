332daab8f2d3b02ae68dce48088512f23596dbd5
Prabhat Singh Tomar <iampstomar@gmail.com>
2020-12-05 21:55:52 +0530

    Implemented a Min Stack

---
+++ Leetcode/min-stack.py
@@ -0,0 +1,66 @@
+# https://leetcode.com/problems/min-stack/
+
+# Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+#
+# push(x) -- Push element x onto stack.
+# pop() -- Removes the element on top of the stack.
+# top() -- Get the top element.
+# getMin() -- Retrieve the minimum element in the stack.
+#
+#
+# Example 1:
+#
+# Input
+# ["MinStack","push","push","push","getMin","pop","top","getMin"]
+# [[],[-2],[0],[-3],[],[],[],[]]
+#
+# Output
+# [null,null,null,null,-3,null,0,-2]
+#
+# Explanation
+# MinStack minStack = new MinStack();
+# minStack.push(-2);
+# minStack.push(0);
+# minStack.push(-3);
+# minStack.getMin(); // return -3
+# minStack.pop();
+# minStack.top();    // return 0
+# minStack.getMin(); // return -2
+#
+#
+# Constraints:
+#
+# Methods pop, top and getMin operations will always be called on non-empty stacks.
+
+class MinStack:
+
+def __init__(self):
+    self.q = []
+
+# @param x, an integer
+# @return an integer
+def push(self, x):
+    curMin = self.getMin()
+    if curMin == None or x < curMin:
+        curMin = x
+    self.q.append((x, curMin));
+
+# @return nothing
+def pop(self):
+    self.q.pop()
+
+
+# @return an integer
+def top(self):
+    if len(self.q) == 0:
+        return None
+    else:
+        return self.q[len(self.q) - 1][0]
+
+
+# @return an integer
+def getMin(self):
+    if len(self.q) == 0:
+        return None
+    else:
+        return self.q[len(self.q) - 1][1]
