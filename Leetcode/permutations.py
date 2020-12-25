fb3091bf8343a1fed48ae1ec17670ea5b27e87b7
Prabhat Singh Tomar <iampstomar@gmail.com>
2020-12-25 21:29:28 +0530

    Simple dfs solution

---
+++ Leetcode/permutations.py
@@ -0,0 +1,36 @@
+# https://leetcode.com/problems/permutations/
+
+# Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+# 
+# Example 1:
+#
+# Input: nums = [1,2,3]
+# Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+# Example 2:
+#
+# Input: nums = [0,1]
+# Output: [[0,1],[1,0]]
+# Example 3:
+#
+# Input: nums = [1]
+# Output: [[1]]
+#
+#
+# Constraints:
+#
+# 1 <= nums.length <= 6
+# -10 <= nums[i] <= 10
+# All the integers of nums are unique.
+
+class Solution:
+    def permute(self, nums):
+        res = []
+        self.dfs(nums, [], res)
+        return res
+
+    def dfs(self, nums, path, res):
+        if not nums:
+            res.append(path)
+            # return # backtracking
+        for i in range(len(nums)):
+            self.dfs(nums[:i] + nums[i + 1:], path + [nums[i]], res)
